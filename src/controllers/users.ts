10ed1c99f7edc3cf28967a21ab8cea1df67c467e
F W <armstrong.wang2000@gmail.com>
2024-06-12 17:35:06 -0600

    Updae user.

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import { deleteUserById, getUsers } from "../db/users";
+import { deleteUserById, getUsers, updateUserById } from "../db/users";
+import { authentication, random } from "../helpers";
 
 export const getAllUsers = async (
   req: express.Request,
@@ -21,6 +22,34 @@ export const deleteUser = async (
 ) => {
   try {
     const user = await deleteUserById(req.params.id);
+    return res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(500);
+  }
+};
+
+export const updateUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { username, password } = req.body;
+    if (!username && !password) {
+      return res.status(400).send("Please specify username and/or password.");
+    }
+
+    const salt = random();
+
+    const values = {
+      ...(username && { username }),
+      ...(password && {
+        authentication: { salt, password: authentication(salt, password) },
+      }),
+    };
+
+    const user = await updateUserById(req.params.id, values);
+
     return res.status(200).json(user);
   } catch (err) {
     console.log(err);
