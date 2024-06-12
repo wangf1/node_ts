8821776b1ba09d2cf986c807fe3fe178a5193453
F W <armstrong.wang2000@gmail.com>
2024-06-12 17:07:16 -0600

    user can only delete self

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -24,3 +24,24 @@ export const isAuthenticated = async (
     return res.sendStatus(403);
   }
 };
+
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, "identity._id") as unknown as string;
+    if (!currentUserId) {
+      return res.sendStatus(403);
+    }
+    if (currentUserId.toString() !== id) {
+      return res.sendStatus(403);
+    }
+    return next();
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(403);
+  }
+};
