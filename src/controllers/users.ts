abdf6e7d3819e4bc54e0cc3c86670a05da6b4d49
F W <armstrong.wang2000@gmail.com>
2024-06-11 23:51:40 -0600

    delete user and readme

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUsers } from "../db/users";
+import { deleteUserById, getUsers } from "../db/users";
 
 export const getAllUsers = async (
   req: express.Request,
@@ -8,6 +8,20 @@ export const getAllUsers = async (
   try {
     const users = await getUsers();
     return res.status(200).json(users);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(500);
+  }
+};
+
+//please implement delete user
+export const deleteUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const user = await deleteUserById(req.params.id);
+    return res.status(200).json(user);
   } catch (err) {
     console.log(err);
     return res.sendStatus(500);
--- src/router/users.ts
+++ src/router/users.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { getAllUsers } from "../controllers/users";
+import { deleteUser, getAllUsers } from "../controllers/users";
 import { isAuthenticated } from "../middlewares";
 
 export default (router: express.Router) => {
   router.get("/users", isAuthenticated, getAllUsers);
+  router.delete("/users/:id", isAuthenticated, deleteUser);
 };
